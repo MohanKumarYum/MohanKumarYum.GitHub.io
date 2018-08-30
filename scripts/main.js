220f7203225b031fe719257c4263f4f3b654dc9b
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-08-30 16:30:07 +1000

    https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics - Supercharging our example website.

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,13 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World!';
+var myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+    var mySrc = myImage.getAttribute('src');
+    if (mySrc === '/images/mozilla-image2.png')
+    {
+        myImage.setAttribute('src','/images/mozilla-image.jpg');
+    }
+    else
+    {
+        myImage.setAttribute('src','/images/mozilla-image2.png');
+    }
+}
