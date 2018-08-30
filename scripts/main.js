bdf44e2f9142a4322ad314f1fcbf0a7c181ce7c5
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-08-30 18:38:30 +1000

    https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics - Completed.

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,33 @@
 var myImage = document.querySelector('img');
 
-myImage.onclick = function(){
+myImage.onclick = function () {
     var mySrc = myImage.getAttribute('src');
-    if (mySrc === '/images/mozilla-image2.png')
-    {
-        myImage.setAttribute('src','/images/mozilla-image.jpg');
+    if (mySrc === '/images/mozilla-image2.png') {
+        myImage.setAttribute('src', '/images/mozilla-image.jpg');
     }
-    else
-    {
-        myImage.setAttribute('src','/images/mozilla-image2.png');
+    else {
+        myImage.setAttribute('src', '/images/mozilla-image2.png');
     }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name. ');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+}
+
+else {
+    var storeName = localStorage.getItem('name');
+    myHeading.textContent = `Mozilla is cool, ${storeName}`;
+}
+
+myButton.onclick = function(){
+    setUserName();
 }
